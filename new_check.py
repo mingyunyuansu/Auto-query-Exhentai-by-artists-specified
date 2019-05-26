55562025839b414fa08022a8fbb91b040e573ba0
Robert Shi <mingyunyuansu@163.com>
2019-05-26 23:04:08 +0800

    Update new_check.py
    让整个过程稍微结构化一点，并加入了简单的异常处理。如果是SSL认证错误导致程序失败，那么直接自动重新开始。

--- new_check.py
+++ new_check.py
@@ -4,9 +4,10 @@ import time
 import json
 from bs4 import BeautifulSoup
 
-#Timing, make sure no problem
-start_time = time.clock()
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+}
 #Prepare cookies, used in requests.get() below
 cookis_f = open('cookie.txt', 'r')
 cookies_copy = eval(cookis_f.read())
@@ -15,11 +16,6 @@ cookis_f.close()
 cookies = {}
 for each_item in cookies_copy:
     cookies[each_item['name']] = each_item['value']
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
-}
-
 
 url = 'https://exhentai.org/'
 
@@ -31,6 +27,7 @@ url1 = r'https://exhentai.org/?f_cats=1017&f_search='
 artists = ['tiramisu', 'kimura neito', 'somejima', 'ichinose', 'pochi-goya', '由浦', 'kazakura', 'jitaku vacation', 'windarteam', 'kuronomiki', 'Nanahara Fuyuki', 'Oohira Sunset',
            'bifidus', 'bang-you', 'lorica', 'sugarbt', 'muneshiro', 'pyon-kti', 'nosebleed', 'komagata', 'ringoya', 'shinozuka yuuji', 'morikawa', 'ma-kurou', 'kyockcho', 'navier haruka', 'akigami satoru', 'shoot the moon', 'Yakitate Jamaica (Aomushi)', 'Neet', 'Brio', '八', '小島', '月野', '鬼月', '靴下', '瓜皮', '黑锅', '直人', '丧尸', 'doumou', 'kaiki', 'kobayashi youkoh',
            '武田', 'okayusan', 'ken-1', 'tonnosuke', 'shioroku', 'meme50', 'type-g', 'kemokomoya', 'ahegao', 'shiina kazuki', 'karasu', 'cyclone', 'kenja time', 'Nasi-pasuya', 'Muchakai', 'PIANIISHIMO', 'gokuburi', 'banana koubou', 'yukiusagi', 'taira issui', 'arakure', 'E-musu', 'gessyu', 'kurowa', 'Funabori Nariaki', 'Orutoro']
+    
 
 def req_by_name(name):
     #get method, loaded by get()
@@ -51,42 +48,59 @@ def req_by_name(name):
     print(name)
     return r.text
 
-old = {} #A dict, artist:[manga's names]
-new = {}
+def main_process():
 
-#Read from old.txt which contains old data to be compared
-file = open('old.txt', 'r') 
-js = file.read()
-old = json.loads(js)   
-file.close()
+    old = {} #A dict, artist:[manga's names]
+    new = {}
 
-cnt = 0 #counting for altered object
-for each_name in artists:
-    html_text = req_by_name(each_name)
-    soup = BeautifulSoup(html_text, features='lxml')
-    collection = []
-    for each in soup.find_all(class_='gl4t glname glink'):
-        #print (each.get_text())
-        collection.append(each.get_text())
-    new[each_name] = collection
-    if each_name not in old.keys() or old[each_name][0] != new[each_name][0]:
-        webbrowser.open(url1+each_name)
-        cnt += 1
-        print('----------------------changed', 'to', new[each_name][0])
+    #Read from old.txt which contains old data to be compared
+    file = open('old.txt', 'r') 
+    js = file.read()
+    old = json.loads(js)   
+    file.close()
 
-js = json.dumps(new)
-f = open('old.txt', 'w')
-f.write(js)
-f.close()
-#routine for gmgard
-webbrowser.open_new('https://gmgard.com')
-print('OK', cnt, 'changed.')
-print('duration', time.clock()-start_time)
+    cnt = 0 #counting for altered object
+    for each_name in artists:
+        html_text = req_by_name(each_name)
+        soup = BeautifulSoup(html_text, features='lxml')
+        collection = []
+        for each in soup.find_all(class_='gl4t glname glink'):
+            #print (each.get_text())
+            collection.append(each.get_text())
+        new[each_name] = collection
+        if each_name not in old.keys() or old[each_name][0] != new[each_name][0]:
+            webbrowser.open(url1+each_name)
+            cnt += 1
+            print('----------------------changed', 'to', new[each_name][0])
 
+    js = json.dumps(new)
+    f = open('old.txt', 'w')
+    f.write(js)
+    f.close()
+    #routine for gmgard
+    webbrowser.open_new('https://gmgard.com')
+    print('Finished', cnt, 'changed.')
 
+def main():
+    start_time = time.clock() #Timing
+    while True:
+        try:
+            main_process()
+            break
+        except SSLError as err:
+            print(err)
+            print(time.clock()-start_time, 'restart')
+        except:
+            print('Unknow error, quit', time.clock()-start_time)
+            break
+        finally:
+            print(time.clock()-start_time)
+
+if __name__=='__main__':
+    main()
 
 '''
-Initialize(or reset) the old.txt by json
+#Initialize(or reset) the old.txt by json
 js = json.dumps(new)
 f = open('old.txt', 'w')
 f.write(js)
